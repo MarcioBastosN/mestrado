edd2358020abf80ab66f55b4a3594057bd673a2e
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2022-02-28 10:48:45 -0300

    layout inicio

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    {{-- <link href = {{ asset("vendor/tws/bootstrap/dist/css/bootstrap.min.css") }} rel="stylesheet" /> --}}
+    {{-- <link href = {{ asset("resources/js/bootstrap.js") }} rel="stylesheet" /> --}}
+</head>
+<body>
+    @yield("body")
+</body>
+</html>
