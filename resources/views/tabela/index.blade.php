7ede22b131ea53f41912fde50366a1653db81946
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2022-03-01 11:36:06 -0300

    ajustes view

--- resources/views/tabela/index.blade.php
+++ resources/views/tabela/index.blade.php
@@ -1,6 +1,8 @@
 @include('layout.app')
 @if ($pontos->where('score', 0)->count() > 0)
-    <h3>Necessario relacionar os pontos aos niveis <a href="{{ route('relacoes_pontos.index')}}">Ir a pagina</a></h3>
+    <div class="alert alert-warning" role="alert">
+        <h3>Necessario relacionar os pontos aos niveis <a href="{{ route('pontos.index') }}">Ir a pagina</a></h3>
+    </div>
 @else
     <table class="table">
         <thead>
@@ -9,35 +11,34 @@
                 <th>x</th>
                 <th>y</th>
                 @foreach ($niveis as $nivel)
-                    <th>Nivel -{{$nivel->nivel}}</th>
+                    <th>Nivel -{{ $nivel->nivel }}</th>
                 @endforeach
                 <th>Indice de Violência</th>
             </tr>
         </thead>
         <tbody>
-        @foreach ($setores as $setor)
-            <tr>
-                <td>{{$setor->setor}}</td>
-                <td>{{$pontos->where('setor', $setor->setor)->first()->X}}</td>
-                <td>{{$pontos->where('setor', $setor->setor)->first()->Y}}</td>
-                @php
-                    $soma = 0;
-                @endphp
-                @foreach ($niveis as $nivel)
-                @php
-                    $temp =0;
-                @endphp
-                    <td>{{
-                    $temp = ($pontos->where('setor', $setor->setor)->where('score', $nivel->score)->sum('score')
-                    / $pontos->where('score', $nivel->score)->sum('score'))}}</td>
+            @foreach ($setores as $setor)
+                <tr>
+                    <td>{{ $setor->setor }}</td>
+                    <td>{{ $pontos->where('setor', $setor->setor)->first()->X }}</td>
+                    <td>{{ $pontos->where('setor', $setor->setor)->first()->Y }}</td>
                     @php
-                        $soma += $temp;
+                        $soma = 0;
                     @endphp
-                @endforeach
-                <td>{{$soma / $nivel->count()}}</td>
-            </tr>
-        @endforeach
+                    @foreach ($niveis as $nivel)
+                        @php
+                            $temp = 0;
+                        @endphp
+                        <td>{{ $temp =
+                            $pontos->where('setor', $setor->setor)->where('score', $nivel->score)->sum('score') / $pontos->where('score', $nivel->score)->sum('score') }}
+                        </td>
+                        @php
+                            $soma += $temp;
+                        @endphp
+                    @endforeach
+                    <td>{{ $soma / $nivel->count() }}</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endif
-
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,37 @@
 @include('layout.app')
+
+<div class="row">
+    <div class="col">
+        <div class="card border-info">
+            <div class="card-body">
+              <h5 class="card-title">Pontos</h5>
+              <p class="card-text">Ocorrências, acrescidas de bairro, setor e pontos
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+    </div>
+
+    <div class="col">
+        <div class="card border-info">
+            <div class="card-body">
+              <h5 class="card-title">Classificaçâo</h5>
+              <p class="card-text">Lista de Tipos de occorencias, para definição dos niveis
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+    </div>
+
+    <div class="col">
+        <div class="card border-info">
+            <div class="card-body">
+              <h5 class="card-title">Niveis</h5>
+              <p class="card-text">relacao nivel pontos
+              </p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+    </div>
+</div>
+
