4976aaede1703217a0961d55d529f15648b40120
MarcioBastos <marciobastosn@gmail.com>
2022-03-02 11:56:35 -0300

    correcoes

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::resource('relacoes_pontos', RelacaoPontosController::class);
 Route::post('relacoes_pontos/loadData', [RelacaoPontosController::class, 'loadData'])->name("relacoes_pontos.load");
 Route::resource('nivel', NivelRelacaoController::class);
 Route::get('tabela', [TabelaController::class, 'index'])->name('tabela');
+Route::get('tabela/download', [TabelaController::class, 'exportTable'])->name('exportTable');
+
+
