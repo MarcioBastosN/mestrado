3c9b97adfe12d79744f4c50482d35502f46f7753
Marcio Andre Neves Bastos <marciobastosn@gmail.com>
2022-03-02 23:45:17 -0300

    ajuste criar tabela, não fanlizado

--- app/Http/Controllers/TabelaController.php
+++ app/Http/Controllers/TabelaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\NivelRelacao;
 use App\Models\Pontos;
-// use Rap2hpoutre\FastExcel\FastExcel;
+use Rap2hpoutre\FastExcel\FastExcel;
 use Illuminate\Http\Request;
 
 class TabelaController extends Controller
@@ -17,19 +17,39 @@ class TabelaController extends Controller
         $pontos = Pontos::all();
 
         return view('tabela.index')->with(compact('setores', "niveis", 'pontos'));
-
     }
 
     public function exportTable()
     {
-        $list = collect([
-            [ 'id' => 1, 'name' => 'Jane' ],
-            [ 'id' => 2, 'name' => 'John' ],
-        ]);
 
+        $setores = Pontos::distinct()->get('setor');
+        $niveis = NivelRelacao::all();
+        $pontos = Pontos::all();
+
+        $linhas = array();
+        foreach ($setores as $setor) {
+            $linha = array('setor' => $setor->setor,
+                    "X" => $pontos->where('setor', $setor->setor)->first()->X,
+                    "Y" => $pontos->where('setor', $setor->setor)->first()->Y);
+            // array_push($linhas, $linha);
+            $soma = 0;
+            $percorreNivel = array();
+            foreach ($niveis as $nivel) {
+                $temp = 0;
+                $temp = $pontos->where('setor', $setor->setor)->where('score', $nivel->score)->sum('score') / $pontos->where('score', $nivel->score)->sum('score');
+                array_push($percorreNivel, ['nivel' => $temp]);
+                $soma += $temp;
+            }
+            array_push($linha, $percorreNivel);
+            $soma / $nivel->count();
+            // array_merge($linhas, ["total" => ($soma / $nivel->count())]);
+            array_push($linhas, $linha);
+        }
+
+
+        $list = collect($linha);
+        // dd($list);
         // return (new FastExcel($list))->export('file.xlsx');
         return (new FastExcel($list))->download('tabela.xlsx');
     }
-
-
 }
